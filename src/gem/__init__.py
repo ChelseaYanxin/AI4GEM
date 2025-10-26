564fa54d3afeb96c03e746e2fd35379c59ef1113
zhangyanxin的macbookpro <zyanxin@zhangyanxindemacbookprodeMacBook-Pro.local>
2025-10-26 16:26:45 +0800

    edit 3d

--- src/gem/__init__.py
+++ src/gem/__init__.py
@@ -1,7 +1,11 @@
 __all__ = [
     "GEMTE",
     "Grid",
+    "GEMTEGraph3D",
+    "GEMTEGraph3DMPNN",
 ]
 
 from .grid import Grid
 from .gem_te import GEMTE
+from .gem_te_3d import GEMTEGraph3D
+from .gem_te_3d_mpnn import GEMTEGraph3DMPNN
